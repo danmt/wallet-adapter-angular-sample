eee6c609d0244246b1682fc9038573e663441b2f
Daniel Marin <danielarturomt@gmail.com>
2021-08-29 13:48:56 +0200

    feat(web-client): generate app backbone

---
+++ apps/web-client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wallet-adapter-test-root',
+  template: `
+    <header>
+      <h1>Wallet adapter test</h1>
+    </header>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'web-client';
+}
